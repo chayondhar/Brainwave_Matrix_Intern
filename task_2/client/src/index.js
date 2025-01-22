ce89d472021a8f66dadbde10f850a8ec56dd9e6a
chayon-dhar <chayondhar08@gmail.com>
2025-01-22 20:11:27 +0600

    create blog section

--- task_2/client/src/index.js
+++ task_2/client/src/index.js
@@ -1,17 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './components/Layout';
+import ErrorPage from './pages/ErrorPage';
+import Home from './pages/Home';
+import PostDetail from './pages/PostDetail';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import UserProfie from './pages/UserProfie';
+import Authors from './pages/Authors';
+import EditPost from './pages/EditPost';
+import CreatePost from './pages/CreatePost';
+import CategoryPosts from './pages/CategoryPosts';
+import AuthorPosts from './pages/AuthorPosts';
+import Dashboard from './pages/Dashboard';
+import Logout from './pages/Logout';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    errorElement: <ErrorPage/>,
+    children: [
+      {index: true, element: <Home/>},
+      {path: "post/:id", element: <PostDetail/>},
+      {path: "register", element: <Register/>},
+      {path: "login", element: <Login/>},
+      {path: "profile/:id", element: <UserProfie/>},
+      {path: "authors", element: <Authors/>},
+      {path: "create", element: <CreatePost/>},
+      {path: "post/:id/categories/:category", element: <CategoryPosts/>},
+      {path: "posts/users/:id", element: <AuthorPosts/>},
+      {path: "myposts/:id", element: <Dashboard/>},
+      {path: "post/:id/edit", element: <EditPost/>},
+      {path: "logout", element: <Logout/>},
+    ]
+  }
+])
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ task_2/client/src/pages/UserProfie.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserProfie = () => {
+  return (
+    <div>UserProfie</div>
+  )
+}
+
+export default UserProfie
